e06eb8c18b8f3e4025fe51220e1fac18ed09528b
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2023-08-01 10:26:33 +0300

    add script to deploy TestERC721 contract

---
+++ deploy/00_TestERC721.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import {getNamedAccounts} from 'hardhat';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/dist/types';
+dotenv.config();
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const {deployments} = hre;
+  const {deploy} = deployments;
+  const {deployer} = await getNamedAccounts();
+
+  await deploy('TestERC721', {
+    from: deployer,
+    args: [],
+    log: true,
+    skipIfAlreadyDeployed: true,
+    contract: 'TestERC721',
+  });
+};
+
+func.tags = ['TestERC721'];
+
+export default func;

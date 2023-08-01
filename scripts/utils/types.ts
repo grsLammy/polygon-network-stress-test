5e978d7c6c8870d50d650f39e010ccdb455ee67c
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2023-08-01 13:00:57 +0300

    feat: add types

---
+++ scripts/utils/types.ts
@@ -0,0 +1,45 @@
+import {ethers} from 'ethers';
+
+export interface GasData {
+  maxFee: number;
+  maxPriorityFee: number;
+}
+
+export interface GasApiResponse {
+  safeLow: {
+    maxPriorityFee: number;
+    maxFee: number;
+  };
+  standard: {
+    maxPriorityFee: number;
+    maxFee: number;
+  };
+  fast: {
+    maxPriorityFee: number;
+    maxFee: number;
+  };
+  estimatedBaseFee: number;
+  blockTime: number;
+  blockNumber: number;
+}
+
+export interface Setup {
+  provider: ethers.providers.InfuraProvider;
+  signer: ethers.Wallet;
+  nonce: number;
+  maxFee: number;
+  maxPriorityFee: number;
+}
+
+export interface MappedReceipt {
+  status?: number;
+  type: number;
+  from: string;
+  to: string;
+  blockHash: string;
+  blockNumber: number;
+  transactionHash: string;
+  cumulativeGasUsed: string;
+  effectiveGasPrice: string;
+  gasUsed: string;
+}
